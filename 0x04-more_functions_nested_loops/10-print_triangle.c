2fbc050b4285998b4b7dc9ce49a0d5cba9ebb77e
abdelghafour_99 <abdelghafour83@live.fr>
2023-09-25 04:55:14 -0700

    0x04. C - More functions, more nested loops commit 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prin triangle
+ *
+ *
+ *@size: the numberof line
+ */
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size > 0)
+	{
+		for (b = 1; b < size; b++)
+		{
+			a = size - b;
+			c = b;
+			while (a > 0)
+			{
+				_putchar(' ');
+				a--;
+			}
+			while (c > 0)
+			{
+				_putchar('#');
+				c--;
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
