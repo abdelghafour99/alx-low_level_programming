f8dc9ead5119f33874825b7f774a0b84fac15512
abdelghafour_99 <abdelghafour83@live.fr>
2023-09-28 11:52:42 -0700

    0x08. C - Recursion commit4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - program 4
+ * returnsthe value of x raised to the power of y
+ * @x: the number
+ * @y: the power
+ * Return: the length of string
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+
+	y--;
+	return (x * _pow_recursion(x, y));
+}
