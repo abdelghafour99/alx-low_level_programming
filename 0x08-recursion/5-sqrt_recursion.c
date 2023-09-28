87299c94023641e4938aad07a62ce402c539b3c8
abdelghafour_99 <abdelghafour83@live.fr>
2023-09-28 12:09:24 -0700

    0x08. C - Recursion commit 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _sqrt_recursion - program 4
+ * returnsthenatural square root of a number
+ * @n: the number
+ * Return: the length of string
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n % 1 != 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+
+	return ((_sqrt_recursion(n) / 2) + n / (2 * _sqrt_recursion(n)));
+}
