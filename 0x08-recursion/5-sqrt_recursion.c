14404f4aebc2e2e3863a4d52ecd6fc2ffb20ad61
abdelghafour_99 <abdelghafour83@live.fr>
2023-09-28 17:09:02 -0700

    0x08. C - Recursion commit 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,17 +4,26 @@
 /**
  * _sqrt_recursion - program 4
  * returnsthenatural square root of a number
+ * sqrt - verify that n=m*m
  * @n: the number
+ * @m: the second number
  * Return: the length of string
  */
 
+int sqrt(int n, int m)
+{
+	if (m == 1)
+		return (-1);
+	if (n == m * m)
+		return (m);
+	return (sqrt(n, m - 1));
+}
 int _sqrt_recursion(int n)
 {
-	if (n % 1 != 0)
+	if (n == 1)
+		return (1);
+	if (n <= 0)
 		return (-1);
-	if (n == 0)
-		return (0);
 
-	return (_sqrt_recursion(n) +
-			(_sqrt_recursion(n) + (n / _sqrt_recursion(n))) / 2);
+	return (sqrt(n, n - 1));
 }
