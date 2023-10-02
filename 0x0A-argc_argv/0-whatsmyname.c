0ccc2cd9aecb686ba0d92f9b7fa41272cd31a955
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-02 15:16:20 -0700

    0x0A. C - argc, argv commit 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - program0
+ *
+ * @argc: number of agrument
+ * @argv: array of arrguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
