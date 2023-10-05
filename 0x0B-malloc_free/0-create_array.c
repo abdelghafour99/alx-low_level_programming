83d947772d9a9d4fcbf57a5f21b02c758a2e95e0
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-04 17:38:24 -0700

    0x0B. C - malloc, free commit 00

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,11 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-		return (NULL);
-
 	char *str;
 	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
 
 	str = malloc(sizeof(str) * size);
 
@@ -23,7 +23,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		str[i] = c
+		str[i] = c;
 
 	return (str);
 
