9c0234a167aa493c7bacf51b61d3d34ca51a7c11
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-05 10:40:12 -0700

    0x0B. C - malloc, free commit 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,18 +15,15 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-	{
 		return (NULL);
-	}
-	else
-	{
-		s = malloc(sizeof(s) * size);
-		if (s != NULL)
-		{
-			for (i = 0; i < size; i++)
-				s[i] = c;
-		}
-	}
+
+	s = malloc(sizeof(s) * size);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
 
 	return (s);
 
