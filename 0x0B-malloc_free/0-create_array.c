d6730c3515c0b9693336ccc036564f5f5fa81e3c
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-04 17:22:17 -0700

    0x0B. C - malloc, free commit 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Null or str.
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size > 0)
+	{
+		char *str;
+
+		str = malloc(sizeof(c));
+		return (str);
+	}
+	return ("NuLL");
+
+}
