301bbc4f190f4ea8cdfdf9ca94f881acd2fdae2a
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-09 16:11:06 -0700

    0x09-static_libraries commit22

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	str = malloc(sizeof(char) * nmemb * size);
 
-	if (block != NULL)
+	if (str != NULL)
 	{
 		for (i = 0; i < (nmemb * size); i++)
 			str[i] = 0;
