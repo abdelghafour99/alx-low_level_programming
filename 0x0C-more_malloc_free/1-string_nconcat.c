20a05a1377770bd5ff9d92a366eac51bf772fe7e
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-09 15:57:37 -0700

    0x09-static_libraries commit10

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,6 +22,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		lena++;
 	while (s1[lenb] != '\0')
 		lenb++;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	str = malloc(sizeof(char) * (lena + lenb + 1));
 	if (str == NULL)
