8d4f7d44affba2fd2ec342b53fdc06af0e026174
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-09 14:42:50 -0700

    0x0C. C - More malloc, free commit 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - allocates memories
+ * using malloc
+ *
+ * @b: the integer givin
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *str;
+
+	str = malloc(sizeof(char) * b);
+	if (str == NULL)
+		str = "98";
+	return (str);
+}
