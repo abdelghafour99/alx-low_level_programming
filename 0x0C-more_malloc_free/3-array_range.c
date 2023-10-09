b07a69e82509b939695611e53d51c80a9bd12840
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-09 16:24:40 -0700

    0x09-static_libraries commit 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,24 +12,24 @@
  * Return: a pointer to the allocated memory
  */
 
-void *array_range(int min, int max)
+int *array_range(int min, int max)
 {
-	char *str;
-	unsigned int i, j;
+	int *ptr;
+	int i, j;
 
 	if (min > max)
 		return (NULL);
 
-	str = malloc(sizeof(int) * ((max - min) + 1));
+	ptr = malloc(sizeof(int) * ((max - min) + 1));
 
-	if (str != NULL)
+	if (ptr != NULL)
 	{
 		for (i = min; i <= max; i++)
 		{
-			str[j] = i;
+			ptr[j] = i;
 			j++;
 		}
-		return (str);
+		return (ptr);
 	}
 	else
 		return (NULL);
