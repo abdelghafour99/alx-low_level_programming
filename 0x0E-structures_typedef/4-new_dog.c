e4a833e9d26de6c57795efc64df14ba9a0e69294
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-10 13:45:42 -0700

    0x0E. C - Structures, typedef commit 44

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,10 +13,11 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d != NULL)
-	{
-	dog_t->name = name;
-	dog_t->iage = age;
-	dog_t->owner = owner;
-	}
+	dog_t *my_dog;
+
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+
+	return (my_dog);
 }
