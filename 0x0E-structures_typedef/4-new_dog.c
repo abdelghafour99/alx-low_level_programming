e3d20c3582bb57ec6f521a7ee405c5358bdd6717
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-10 13:39:07 -0700

    0x0E. C - Structures, typedef commit 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * new_dog - Initialize a dog structure
+ *
+ *@name: the name to attribut
+ *@age: the age to attribut
+ *@owner: the owner to attribut
+ *
+ *Return: always d
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+	dog_t->name = name;
+	dog_t->iage = age;
+	dog_t->owner = owner;
+	}
+}
