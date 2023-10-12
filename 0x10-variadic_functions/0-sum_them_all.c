39d571c605dccef35352ece75d179b3073cf7fdd
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-12 10:46:18 -0700

    0x10. C - Variadic functions commit 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * sum_them_all - returns the sum of all its parameters
+ *
+ *
+ * @n: parametre
+ * Return: sum
+ **/
+
+int
+sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum;
+
+	va_start(ap, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+	va_end(ap);
+	return (sum);
+}
