0289ffe41dde17853e8772737d426a6adbd17de9
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-24 11:20:44 -0700

    0x13. C - More singly linked lists commit 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_listint2 - function that frees a list
+ *
+ * @head: list of element
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *l = *head;
+	listint_t *m;
+
+	while (l != NULL)
+	{
+		m = l->next;
+		free(l);
+		l = m;
+	}
+	*head = NULL;
+}
