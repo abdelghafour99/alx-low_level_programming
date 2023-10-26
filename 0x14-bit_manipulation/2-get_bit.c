286bad2cde625413c14f7fd46d2d2980703fab7b
abdelghafour_99 <abdelghafour83@live.fr>
2023-10-26 15:12:33 -0700

    0x14. C - Bit manipulation commit 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -15,13 +15,16 @@ int get_bit(unsigned long int n, unsigned int index)
 	int Arr[100];
 	unsigned int i = 0;
 
+	if (index < i)
+		return (-1);
 	while (n > 0)
 	{
 		Arr[i] = n % 2;
 		n = n / 2;
 		i++;
 	}
-	if ((index > i) || (index < 0))
+	if (index > i)
 		return (-1);
+
 	return (Arr[index]);
 }
