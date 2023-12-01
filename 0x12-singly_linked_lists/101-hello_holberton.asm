8bdb40ccde0df0675f3b9e67daf66729f5265836
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-01 09:19:45 -0800

    0x12-singly_linked_lists 101101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,9 @@
 /*             it's64-bit program in assembly                       */
 /*     that prints Hello, Holberton, followed by a new line        */
 
-section .data
-	msg db "Hello, Holberton", 0
-	fmt db "%s", 10, 0
 
 section .text
 	global main
-	extern printf
 
 main:
 	mov rax, 1
