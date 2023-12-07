7d69955dc9cfef5cedfc5ad21d9f61aa6aae81ac
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-07 14:35:26 -0800

    0x17-doubly_linked_lists commit 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * free_dlistint - function that frees a dlist
+ *
+ * @head: dlist of element
+ **/
+
+void free_dlistint(list_t *head)
+{
+	dlistint_t *l;
+
+	while (head)
+	{
+		l = head->next;
+		free(head);
+		head = l;
+	}
+}
