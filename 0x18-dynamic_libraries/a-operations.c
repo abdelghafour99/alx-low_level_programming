3ba8c30b2a622292f9fcd2354503ed0875b333c7
abdelghafour_99 <abdelghafour83@live.fr>
2023-12-11 13:53:05 -0800

    0x18-dynamic_libraries commit 100

---
+++ 0x18-dynamic_libraries/a-operations.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * add - addition
+ * @a : the first number
+ * @b : the second number
+ * Return: a + b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+
+}
+
+/**
+ * sub - sub
+ * @a : the first number
+ * @b : the second number
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a + b);
+
+}
+
+/**
+ * mul - multiplication
+ * @a : the first number
+ * @b : the second number
+ * Return: a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
+
+/**
+ * div - division
+ * @a : the first number
+ * @b : the second number
+ * Return: a / b
+ */
+int div(int a, int b)
+{
+	if (b)
+		return (a / b);
+}
+
+/**
+ * mod - modolus
+ * @a : the first number
+ * @b : the second number
+ * Return: a % b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
